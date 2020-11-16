79d955b8c20694640599632f0537749d1248d45e
Бажен Левковець <bazhen.paseka@gmail.com>
2020-11-16 18:02:57 +0200

    read data by HAL_GPIO_ReadPin

--- 93c86-eeprom-sm.c
+++ 93c86-eeprom-sm.c
@@ -16,6 +16,7 @@
 */
 
 	#include "93c86-eeprom-sm.h"
+	#include "usart.h"
 
 /*
 **************************************************************************
@@ -126,7 +127,7 @@ void WRITE (void) 	{
 	}
 	delaydd(500);
 
-	write_u16 = 0x6666 ;
+	write_u16 = 0x5b9e ;
 	for (int pos = 0; pos<16; pos++) {
 		if ( CHECK_BIT(write_u16, pos) == 0 ) {
 			HAL_GPIO_WritePin(DI_GPIO_Port, DI_Pin, RESET ) ;
@@ -200,22 +201,36 @@ void READ (void)	{
 		delaydd(500);
 		HAL_GPIO_WritePin(CLK_GPIO_Port, CLK_Pin,   SET ) ;
 		delaydd(1000);
+
 		HAL_GPIO_WritePin(CLK_GPIO_Port, CLK_Pin, RESET ) ;
 		delaydd(500);
 	}
-	delaydd(500);
+	delaydd(1500);
 
-	for (int pos = 0; pos<20; pos++) {
+	#define BITS_QNT	15
+	GPIO_PinState memory_bit_ps[BITS_QNT];
+
+	for (int pos = BITS_QNT; pos >= 0 ; pos--) {
 		HAL_GPIO_WritePin(CLK_GPIO_Port, CLK_Pin,   SET ) ;
 		delaydd(1000);
 		HAL_GPIO_WritePin(CLK_GPIO_Port, CLK_Pin, RESET ) ;
+		memory_bit_ps[pos] =  HAL_GPIO_ReadPin(DO_GPIO_Port, DO_Pin);
 		delaydd(1000);
 	}
 
 	delaydd(1000);
     HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, RESET ) ;
+
+	#define	DEBUG_STRING_SIZE2		100
+    char DebugString2[DEBUG_STRING_SIZE2];
+
+	for (int pos = 0; pos < BITS_QNT ; pos++) {
+	sprintf(DebugString2," %d", (int)memory_bit_ps[pos]);
+	HAL_UART_Transmit(&huart2, (uint8_t *)DebugString2, strlen(DebugString2), 100);
+	}
 }
 //*** READ *******************************************************
+
 
 /*
 **************************************************************************
